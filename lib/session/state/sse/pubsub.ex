09f116423cddfa247bf04fb32ac047950b6d5c7d
Renato Massaro <renato@hackerexperience.com>
2018-11-20 05:13:40 -0200

    Merge SSESub and SSEPub into SSEPubSub

--- lib/session/state/sse/pubsub.ex
+++ lib/session/state/sse/pubsub.ex
@@ -1,8 +1,10 @@
-defmodule Helix.Session.State.SSE.Sub do
+defmodule Helix.Session.State.SSE.PubSub do
 
   use GenServer
 
   alias Helix.Session.Action.SSE, as: SSEAction
+  alias Helix.Session.Model.SSE
+  alias Helix.Session.Query.Session, as: SessionQuery
   alias Helix.Session.State.Session.API, as: SessionStateAPI
   alias Helix.Session.State.SSE.API, as: SSEStateAPI
   alias Helix.Session.Repo
@@ -28,6 +30,54 @@ defmodule Helix.Session.State.SSE.Sub do
 
   def start_link,
     do: GenServer.start_link(__MODULE__, [], name: @registry_name)
+
+  def publish(:global, payload),
+    do: pg_notify("sse_queue_all", payload)
+
+  def publish(domains, event) do
+    message_id = SSE.Queue.generate_message_id()
+
+    nodes =
+      domains
+      |> SessionQuery.get_domains_sessions()
+      |> sort_by_node()
+
+    cluster_queue_data =
+      Enum.reduce(nodes, %{}, fn {node_id, sessions}, acc ->
+        notifications =
+          Enum.map(sessions, fn session_id ->
+            %{
+              session_id: session_id,
+              message_id: SSE.Queue.generate_message_id(),
+              event: event
+            }
+          end)
+
+        node_queue_data =
+          Enum.reduce(notifications, [], fn notification, acc ->
+            [{notification.message_id, notification.session_id} | acc]
+          end)
+
+        pg_notify("sse_queue_" <> node_id, notifications)
+        Map.put(acc, node_id, node_queue_data)
+      end)
+
+    SSEAction.bulk_insert_in_queue(cluster_queue_data)
+  end
+
+  defp sort_by_node(domains_sessions) do
+    Enum.reduce(domains_sessions, %{}, fn {node_id, session_id}, acc ->
+      current = acc[node_id] || []
+      Map.put(acc, node_id, [session_id | current])
+    end)
+  end
+
+  defp pg_notify(channel, notification) do
+    notification_str = Poison.encode!(notification)
+    Ecto.Adapters.SQL.query(
+      Repo, "NOTIFY \"#{channel}\", '#{notification_str}'"
+    )
+  end
 
   # Callbacks
 
@@ -67,7 +117,9 @@ defmodule Helix.Session.State.SSE.Sub do
   end
 
   def handle_info({:notification, _, _, _channel_name, raw}, state) do
+    IO.puts "WILL NOTIFY FY FY"
     notifications = parse_notification(raw)
+    IO.inspect(notifications)
 
     ids_sent =
       Enum.reduce(notifications, [], fn notification, acc ->
